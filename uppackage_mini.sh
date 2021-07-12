ab96c72d2848a7683309080f00a7329fccecd9b7
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-07-12 23:12:39 +0800

    915

--- uppackage_mini.sh
+++ uppackage_mini.sh
@@ -29,6 +29,9 @@ echo '---------------------------------' >> ./package/base-files/files/etc/banne
 #echo  'CONFIG_NVME_FC=y' >> ./package/target/linux/x86/config-5.4
 #echo  'CONFIG_NVME_MULTIPATH=y' >> ./package/target/linux/x86/config-5.4
 #echo  'CONFIG_NVME_TCP=y' >> ./package/target/linux/x86/config-5.4
+for X in $(ls -1 target/linux/x86 | grep "config-"); do echo -e "\n$(cat ./package/build/DRM-I915)" >> target/linux/x86/${X}; done
+# sed -i "/dns_caching_dns/d" $(PKG_Finder d package luci-app-turboacc)/root/etc/config/turboacc
+# echo "	option dns_caching_dns '223.5.5.5,114.114.114.114'" >> $(PKG_Finder d package luci-app-turboacc)/root/etc/config/turboacc
 
 ./scripts/feeds update -i
 # 生成默认配置及缓存
