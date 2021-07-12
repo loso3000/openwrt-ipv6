ee644b9725de7d2628a5aa0c4e87aa30fe665510
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-07-13 00:12:25 +0800

    ssrbz

--- initipv6.sh
+++ initipv6.sh
@@ -217,6 +217,9 @@ find ./ -name *.rej | xargs rm -f
 # Add execute permission for ipv6-helper
 #chmod +x /bin/ipv6-helper
 
+sed -i 's+"), 10)/+"), 1)/g' ./package/lean/luci-app-ssr-plus//luasrc/controller/shadowsocksr.lua  #shadowsocksr
+sed -i 'sh"), 50)/h"), 9)/g' ./package/diy/luci-app-openclash/luasrc/controller/openclash.lua   #openclash
+
 PKG_Finder() {
 	local Result
 	[[ $# -ne 3 ]] && {
